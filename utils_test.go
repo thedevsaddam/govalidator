e093a2b2b83685d8f628908091071d2fbd3e022a
Saddam H <thedevsaddam@gmail.com>
2017-10-11 17:38:27 +0600

    Fix isEmpty slice test

--- utils_test.go
+++ utils_test.go
@@ -55,17 +55,17 @@ func Test_isEmpty(t *testing.T) {
 	var Int8 int
 	var Float32 float32
 	var Str string
-	var Array [4]int
+	var Slice []int
 	list := map[string]interface{}{
 		"_int":     Int,
 		"_int8":    Int8,
 		"_float32": Float32,
 		"_str":     Str,
-		"_array":   Array,
+		"_slice":   Slice,
 	}
 	for k, v := range list {
 		if !isEmpty(v) {
-			t.Errorf("%s failed", k)
+			t.Errorf("%v failed", k)
 		}
 	}
 }
